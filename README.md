8665431addaaa099b5ec769ea102b149a4e9867c
Ryan Oliveira <oliveira.ryan@outlook.com>
2021-11-15 23:32:27 -0500

    added windows noises

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # YellDetector
 
+## **Important Note:**  
+Currently only works on Windows in PowerShell
+
+## Instructions:
+The script will run continuously until a keyboard interrupt (Ctrl+C) is issued.
+
+Currently, the normalized input volume will be printed in the terminal. If the value is over 200, a Windows Noise will play.
+
 ## Useful links:
 * [Sounddevice docs](https://python-sounddevice.readthedocs.io/en/0.4.3/)
+
+## TODO:
+* Make printing of input volume be a parse command (default: off)
+* Make the value that a windows noise is made be customizable
+* Log the yells!
+  * Probably into a CSV
+* Make the script Linux compatible
+  * Possibly a Linux version
+* Clean up the code so it isn't just a straight script
+  * i.e. `if __name__ == "__main__"`
--- audio_reader.py
+++ audio_reader.py
@@ -2,10 +2,29 @@
 
 import sounddevice as sd
 import numpy as np
+import os
+import sys
+import time
+# In Powershell, this creates the windows noise 
+cmd = "echo " + chr(7)
+
+# Duration in seconds
+# duration = 10
 
 def print_sound(indata, outdata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
-    print ("|" * int(volume_norm))
+    # print ("|" * int(volume_norm))
+    print(int(volume_norm))
+    if (volume_norm >= 200):
+        os.system(cmd)
+        # sys.exit(0)
 
-with sd.Stream(callback=print_sound):
-    sd.sleep(10000)
+try: 
+    with sd.Stream(callback=print_sound):
+        # sd.sleep(duration * 1000)
+        # print('press Ctrl+C to stop the recording')
+        while True:
+            time.sleep(60)
+
+except KeyboardInterrupt:
+    sys.exit('Interrupted by user')
