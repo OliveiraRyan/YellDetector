66e371508b840eb7ffedcf4464ab789eca60fae5
Ryan Oliveira <oliveira.ryan@outlook.com>
2021-11-16 15:29:45 -0500

    added delay between yells and a yell counter

--- audio_reader.py
+++ audio_reader.py
@@ -8,23 +8,29 @@ import time
 # In Powershell, this creates the windows noise 
 cmd = "echo " + chr(7)
 
-# Duration in seconds
-# duration = 10
+# Duration in seconds between 'yells'
+duration = 1
+
+yell_counter = 0
 
 def print_sound(indata, outdata, frames, time, status):
+    global yell_counter
     volume_norm = np.linalg.norm(indata)*10
+
     # print ("|" * int(volume_norm))
-    print(int(volume_norm))
-    if (volume_norm >= 200):
+    # print(int(volume_norm))
+
+    if (volume_norm >= 50): # 180 seems like a good start
         os.system(cmd)
-        # sys.exit(0)
+        yell_counter += 1
+        sd.sleep(duration * 1000)
 
 try: 
-    with sd.Stream(callback=print_sound):
+    with sd.Stream(blocksize=0, callback=print_sound):
         # sd.sleep(duration * 1000)
-        # print('press Ctrl+C to stop the recording')
+        print('press Ctrl+C to stop the recording')
         while True:
             time.sleep(60)
 
 except KeyboardInterrupt:
-    sys.exit('Interrupted by user')
+    sys.exit('Interrupted by user, you yelled {} times'.format(yell_counter))
