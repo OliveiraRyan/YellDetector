8aacfa7be68a9cc4d7d1f0c64212e8ce5a6a8e3c
Ryan Oliveira <oliveira.ryan@outlook.com>
2021-11-18 21:51:27 -0500

    added optional args and cleaned up some code

--- README.md
+++ README.md
@@ -8,12 +8,27 @@ The script will run continuously until a keyboard interrupt (Ctrl+C) is issued.
 
 Currently, the normalized input volume will be printed in the terminal. If the value is over 200, a Windows Noise will play.
 
+### Optional Arguments
+
+-h, --help &mdash; show this help message and exit  
+-vol, --volume &mdash; normalized volume value that triggers a 'yell'  
+-v {0,1,2}, --verbosity {0,1,2} &mdash; how verbose the program is:
+* 0 = no printed output
+* 1 = outputs audio bars
+* 2 = outputs normalized volume values
+
+-d, --duration &mdash; time (in seconds) in between 'yells' (default 1 second)  
+-nr, --NoRecord &mdash; disables recording data into a .csv file
+
 ## Useful links:
 * [Sounddevice docs](https://python-sounddevice.readthedocs.io/en/0.4.3/)
 
 ## TODO:
-* Make printing of input volume be a parse command (default: off)
-* Make the value that a windows noise is made be customizable
+* ~~Make printing of input volume be a parse command (default: off)~~
+* Make the volume level that triggers a 'yell' made be customizable
+  * ~~Make it a parse command~~
+  * Set levels instead of a specified number (e.g. low/med/high)
+  * Also give option to set a specific number
 * Log the yells!
   * Probably into a CSV
 * Make the script Linux compatible
--- audio_reader.py
+++ audio_reader.py
@@ -1,26 +1,54 @@
-# Print out realtime audio volume as ascii bars
-
 import sounddevice as sd
 import numpy as np
 import os
 import sys
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description="Beeps at you when you\'re too loud", 
+                                 formatter_class=argparse.RawTextHelpFormatter)
+# needs to be renamed and able to set to a choice of low/med/high or something similar
+# e.g. -s (sensitivity), -l (level)
+parser.add_argument("-vol", "--volume", help="normalized volume value that triggers a 'yell'",
+                    type=int, default=180)
+parser.add_argument("-d", "--duration", type=int, default=1,
+                    help="time (in seconds) in between 'yells' (default 1 second)")
+parser.add_argument("-v", "--verbosity", type=int, default=0, choices=[0,1,2], 
+                    help="""how verbose the program is:
+    0 = no printed output
+    1 = outputs audio bars
+    2 = outputs normalized volume values""")
+parser.add_argument("-nr", "--NoRecord", help="disables recording data into a .csv file", action="store_true")
+
+args = parser.parse_args()
+
+
+# normalized volume value that triggers a 'yell' (Default 180)
+volume = args.volume
+
+# Duration in seconds between 'yells' (Default 1 second)
+duration = args.duration
+
+# Determines the verbosity of the printed output
+# 0 = no output, 1 = bars, 2 = exact values
+verbosity = args.verbosity
+
 # In Powershell, this creates the windows noise 
 cmd = "echo " + chr(7)
 
-# Duration in seconds between 'yells'
-duration = 1
-
 yell_counter = 0
+
 
 def print_sound(indata, outdata, frames, time, status):
     global yell_counter
     volume_norm = np.linalg.norm(indata)*10
 
-    # print ("|" * int(volume_norm))
-    # print(int(volume_norm))
+    if (verbosity == 1):
+        print ("|" * int(volume_norm))
+    elif (verbosity == 2):
+        print(int(volume_norm))
 
-    if (volume_norm >= 50): # 180 seems like a good start
+    if (volume_norm >= volume): # 180 seems like a good start
         os.system(cmd)
         yell_counter += 1
         sd.sleep(duration * 1000)
@@ -33,4 +61,6 @@ try:
             time.sleep(60)
 
 except KeyboardInterrupt:
+    if (args.NoRecord):
+        sys.exit('Interrupted by user')
     sys.exit('Interrupted by user, you yelled {} times'.format(yell_counter))
